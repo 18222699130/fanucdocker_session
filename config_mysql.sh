27f6b962c52a36916da2bcb67dc6abe661a6642d
brand_process <lsjfy0411@163.com>
2019-01-24 19:42:29 +0800

    Update config_mysql.sh

--- config_mysql.sh
+++ config_mysql.sh
@@ -3,6 +3,10 @@ spawn mysql -u root -h 127.0.0.1 -p
 expect "Enter password: "
 send "fanuc123\r\n"
 expect eof
-send "create fanucdocker;\r\n"
+send "create database fanucdocker;\r\n"
 send "use fanucdocker;\r\n"
-send "source ~/fanucdocker_session/fanucdocker_session.sql;\r\n"	
+send "set names utf8"
+send "source ~/fanucdocker_session/fanucdocker_session.sql;\r\n"
+send "exit\r\n"
+expect eof
+interact
